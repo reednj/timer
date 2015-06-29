21bd9285ad9aa5852e390c04fc18145ea11ebf33
Nathan Reed <reednj@gmail.com>
2015-06-29 19:53:46 +0800

    fix install-hooks script

--- config/deploy/install-hook.sh
+++ config/deploy/install-hook.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+# hopefully get back to the app root directory
+cd ../..
+
 # create the remote repo, set the correct flag, and copy over the pr hook
-cp ./post-receive.rb .git/hooks/post-receive
+cp ./config/deploy/post-receive.rb .git/hooks/post-receive
 chmod 711 .git/hooks/post-receive
-chmod 711 pr-deploy.sh
+chmod 711 ./config/deploy/pr-deploy.sh
 
 # make sure it will let us push
 git config receive.denyCurrentBranch ignore
