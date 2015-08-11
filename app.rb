7fc5f3bc301b77fa8e15b0ef4ef4cee418163ece
Nathan Reed <reednj@gmail.com>
2015-08-11 18:45:39 +0800

    redirect to a random page on load

--- app.rb
+++ app.rb
@@ -6,7 +6,15 @@ require 'json'
 require 'yaml'
 require 'erubis'
 
+require 'securerandom'
+
+
 get '/' do
+	key = SecureRandom.hex(4)
+	redirect to("/t/#{key}")
+end
+
+get '/t/:key' do |key|
 	erb :index
 end
 
